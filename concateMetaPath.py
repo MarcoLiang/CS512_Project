be42a9ab9afd419fe49045fc973966fc27e30f71
Marco Liang <marcoliangbing@gmail.com>
2018-03-23 14:42:16 -0500

    update

--- concateMetaPath.py
+++ concateMetaPath.py
@@ -27,7 +27,7 @@ class concatMetaPath:
                 if curr_id in self.authorPair_pattern[author_pair]:
                     self.authorPair_pattern[author_pair][curr_id] += 1
                 else:
-                    self.authorPair_pattern[author_pair][curr_id] = 0
+                    self.authorPair_pattern[author_pair][curr_id] = 1
 
                 #
                 #
@@ -37,12 +37,31 @@ class concatMetaPath:
                 #     self.pattern_id[pattern] = p_id
                 #     p_id += 1
 
-    def concatePath(self, prev_path_dir, length):
+    def write_file_l1(self, file):
+        for pair, pattern_dict in self.authorPair_pattern.items():
+            a_pair = [retrieve_id(e) for e in pair]
+            for pattern, cnt in pattern_dict.items():
+                lst = list(map(str, [a_pair[0], pattern, a_pair[1], cnt]))
+                file.write('\t'.join(lst) + '\n')
+
+    def write_pattern_id(self, file):
+        for k, v in self.pattern_id.items():
+            file.write(str(k) + '\t' + str(v) + '\n')
+
+    def concatePath(self, prev_file, l1_file, out_file):
         '''
         :param prev_path_dir: the direction of path length - 1
         :param length: the length of path need to be generate
         '''
-        assert length > 1
+        for line_prev in prev_file:
+            for line_l1 in l1_file:
+                toks_prev = line_prev.strip().split("\t")
+                toks_l1 = line_l1.strip().split("\t")
+                if toks_prev[-2] == toks_l1[0]:
+                    new_path = toks_prev[:-1] + toks_l1[1:-1] + [int(toks_prev[-1]) * int(toks_l1[-1])]
+                    new_path = list(map(str, new_path))
+                    out_file.write('\t'.join(new_path) + '\n')
+
 
 
 
@@ -51,9 +70,15 @@ class concatMetaPath:
 meta = concatMetaPath()
 file = open("_reduced_dataset/pattern/meta_path_pattern_l1_all.txt")
 meta.build_dict("_reduced_dataset/pattern/meta_path_pattern_l1_all.txt")
-print(meta.authorPair_pattern)
-
-
+# print(meta.authorPair_pattern)
+# l1_file = open("_reduced_dataset/filter_venue_since_2005/pattern/meta-path_pattern_l1", 'w')
+# meta.write_pattern_id(file)
+l1_file = open("_reduced_dataset/filter_venue_since_2005/pattern/meta-path_pattern_l1")
+# l2_file = open("_reduced_dataset/filter_venue_since_2005/pattern/meta-path_pattern_l2", 'w')
+# meta.write_file_l1(file)
+l2_file = open("_reduced_dataset/filter_venue_since_2005/pattern/meta-path_pattern_l2")
+l3_file = open("_reduced_dataset/filter_venue_since_2005/pattern/meta-path_pattern_l3", 'w')
+meta.concatePath(l2_file, l1_file, l3_file)
 #
 # for pair in meta.author_pairs:
 #     print( int(pair[0]) < int(pair[1]))
