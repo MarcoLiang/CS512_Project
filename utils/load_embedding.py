575aaee7559e2c8247c3974e0aeaca317e8dfaf2
Ruiyang Wang <reyoungwang@gmail.com>
2018-04-17 12:33:44 -0500

    functional

--- utils/load_embedding.py
+++ utils/load_embedding.py
@@ -1,16 +1,16 @@
 import codecs
+from collections import Counter
 
 from gensim.models import KeyedVectors
 import numpy as np
 import torch
 import torch.nn as nn
 
-author_embeddings = dict()
-author_id = dict()
 
 
 # id file has format <id"\t"name>
 def load_id_file(file_path, mode='m2v'):
+    author_id = dict()
     if mode == 'm2v':
         with codecs.open(id_path, 'r', 'utf-8') as f:
             for line in f.readlines():
@@ -41,10 +41,12 @@ def load_id_file(file_path, mode='m2v'):
                     print("Warning: duplicate paper {}".format(line[1]))
                 author_id[line[1].replace(' ', '_')] = int(line[0])
         f.close()
+    return author_id
 
-def load_pre_trained_emb(emb_path, mode='m2v'):
+def embedding_loader(id_path, emb_path, mode='m2v'):
+    author_id = load_id_file(id_path, mode)
     word_vectors = KeyedVectors.load_word2vec_format(emb_path, binary=True)
-    emb_matrix = np.zeros([len(author_id) + 1, word_vectors.vector_size])
+    emb_matrix = np.zeros([len(author_id), word_vectors.vector_size])
     cnt = 0
     for i, v in enumerate(word_vectors.index2word):
         if v not in author_id:
@@ -54,12 +56,10 @@ def load_pre_trained_emb(emb_path, mode='m2v'):
         emb_matrix[author_id[v]] = word_vectors.vectors[i]
     return emb_matrix
 
-
 if __name__ == "__main__":
     id_path = "/Users/ruiyangwang/Desktop/ResearchProject/CS512_Project/data/focus/venue_filtered_unique_id"
     emb_path = "/Users/ruiyangwang/Desktop/ResearchProject/ESim/results/vec.dat"
-    load_id_file(id_path, "esim")
-    m = load_pre_trained_emb(emb_path)
+    m = embedding_loader(id_path, emb_path, "esim")
     torch_emb = nn.Embedding(m.shape[0], m.shape[1])
     torch_emb.weight.data.copy_(torch.from_numpy(m))
     print("Done")
