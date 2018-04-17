97d847b57e68d44993126b5044f77a63a5d5b577
Ruiyang Wang <reyoungwang@gmail.com>
2018-04-16 21:47:41 -0500

    Edit_loader_add_esim_emb

--- utils/load_embedding.py
+++ utils/load_embedding.py
@@ -8,35 +8,57 @@ import torch.nn as nn
 author_embeddings = dict()
 author_id = dict()
 
+
 # id file has format <id"\t"name>
-def load_id_file(id_path, mode='m2v'):
+def load_id_file(file_path, mode='m2v'):
     if mode == 'm2v':
-        with codecs.open(id_path,'r', 'utf-8') as f:
+        with codecs.open(id_path, 'r', 'utf-8') as f:
             for line in f.readlines():
                 line = line.strip().split("\t")
                 if "a" + line[1] in author_id:
                     print("Warning: duplicate author {}".format(line[1]))
                 author_id["a" + line[1].replace(' ', '')] = int(line[0])
+        f.close()
     elif mode == 'esim':
-        pass
+        with codecs.open(id_path + '/id_author.txt', 'r', 'utf-8') as f:
+            for line in f.readlines():
+                line = line.strip().split("\t")
+                if  line[1] in author_id:
+                    print("Warning: duplicate author {}".format(line[1]))
+                author_id[line[1].replace(' ', '_')] = int(line[0])
+        f.close()
+        with codecs.open(id_path + '/id_conf.txt', 'r', 'utf-8') as f:
+            for line in f.readlines():
+                line = line.strip().split("\t")
+                if line[1] in author_id:
+                    print("Warning: duplicate conf {}".format(line[1]))
+                author_id[line[1].replace(' ', '_')] = int(line[0])
+        f.close()
+        with codecs.open(id_path + '/paper.txt', 'r', 'utf-8') as f:
+            for line in f.readlines():
+                line = line.strip().split("\t")
+                if line[1] in author_id:
+                    print("Warning: duplicate paper {}".format(line[1]))
+                author_id[line[1].replace(' ', '_')] = int(line[0])
+        f.close()
 
 def load_pre_trained_emb(emb_path, mode='m2v'):
     word_vectors = KeyedVectors.load_word2vec_format(emb_path, binary=True)
     emb_matrix = np.zeros([len(author_id) + 1, word_vectors.vector_size])
-    cnt = 0;
+    cnt = 0
     for i, v in enumerate(word_vectors.index2word):
-        if v.startswith("a"):
-            if v not in author_id:
-                print(v)
-                continue
-            cnt +=1
-            emb_matrix[author_id[v]] = word_vectors.vectors[i]
+        if v not in author_id:
+            print(v)
+            continue
+        cnt += 1
+        emb_matrix[author_id[v]] = word_vectors.vectors[i]
     return emb_matrix
 
+
 if __name__ == "__main__":
-    id_path = "/Users/ruiyangwang/Desktop/ResearchProject/net_dblp/output/focus/venue_filtered/id_author.txt"
-    emb_path = "/Users/ruiyangwang/Desktop/ResearchProject/CS512_Project/embedding_file/m2v/dblp_reduced_focus.cac.w1000.l100"
-    load_id_file(id_path)
+    id_path = "/Users/ruiyangwang/Desktop/ResearchProject/CS512_Project/data/focus/venue_filtered_unique_id"
+    emb_path = "/Users/ruiyangwang/Desktop/ResearchProject/ESim/results/vec.dat"
+    load_id_file(id_path, "esim")
     m = load_pre_trained_emb(emb_path)
     torch_emb = nn.Embedding(m.shape[0], m.shape[1])
     torch_emb.weight.data.copy_(torch.from_numpy(m))
